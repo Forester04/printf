d767feb7d0ae17af9ee2ee1b5308a236510d7729
Foerster04 <erickforester2018@gmail.com>
2022-08-09 22:46:25 +0000

    base converters

---
+++ base_converters.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output,
+		unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
+
+/**
+ * convert_sbase - Converts a signed long to an inputted base and stores
+ *                 the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: A signed long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec)
+{
+	int size;
+	char digit, pad = '0';
+	unsigned int ret = 1;
+
+	for (size = 0; *(base + size);)
+		size++;
