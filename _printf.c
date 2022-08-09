b935717fa57e61e033407130eae80e85805e4e88
totty-stack <amimokoths97@gmail.com>
2022-08-09 15:41:53 -0700

    printf update

--- _printf.c
+++ _printf.c
@@ -51,3 +51,41 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 				ret += f(args, output, flags, wid, prec, len);
 				continue;
 			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
+}
+
