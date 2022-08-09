b55f5897af545962868ae1ce5b8677d0cf90beb9
totty-stack <amimokoths97@gmail.com>
2022-08-09 15:55:52 -0700

    base converters update

--- base_converters.c
+++ base_converters.c
@@ -27,3 +27,41 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 
 	for (size = 0; *(base + size);)
 		size++;
+	if (num >= size || num <= -size)
+		ret += convert_sbase(output, num / size, base,
+			flags, wid - 1, prec - 1);
+
+	else
+	{
+		for (; prec > 1; prec--, wid--) /* Handle precision */
+			ret += _memcpy(output, &pad, 1);
+
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+	}
+	digit = base[(num < 0 ? -1 : 1) * (num % size)];
+	_memcpy(output, &digit, 1);
+
+	return (ret);
+}
+
+/**
+ * convert_ubase - Converts an unsigned long to an inputted base and
+ *               stores the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: An unsigned long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+	unsigned char flags, int wid, int prec)
+{
